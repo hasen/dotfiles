53ab5758183716096e4ea4eeb0efc87ba033c0f5
root <hasenfus7@gmail.com>
2013-06-27 07:58:58 +0900

    add cua-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -207,11 +207,24 @@
 ;; howm-menuの言語を日本語に
 ;; ( setq howm-menu-lang 'ja )
 ;; howmメモを１日１ファイルにする
-( setq howm-file-name-format "%Y/%m/%Y-%m-%d.howm" )
+; ( setq howm-file-name-format "%Y/%m/%Y-%m-%d.howm" )
 ;; howm-modeを読み込む
 ( when( require 'howm-mode nil t )
   ;; C-c,,で起動
   ( define-key global-map ( kbd "C-c ,," ) 'howm-menu ))
+;; howmを保存と同時に閉じる
+( defun howm-save-guffer-and-kill ()
+  ( interactive )
+  ( when( and( buffer-file-name )
+             ( string-match " \\.howm" ( buffer-file-name )))
+    ( save-buffer)
+    ( kill-buffer nil )))
+;; C-c C-cでメモの保存と同時にバッファを閉じる
+( define-key howm-mode-map ( kbd "C-c C-c" ) 'howm-save-buffer-and-kill )
+
+;; cua-modeの設定 矩形編集モードのみ利用する
+( cua-mode t )
+( setq cua-enable-cua-keys nil )
 
 
 ;;; SHORT-CUT
